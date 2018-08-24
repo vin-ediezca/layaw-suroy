5de3cc8bd0d89a7d8822e8e35a11156bc20de671
kierra-addison <vinediezca@gmail.com>
2018-08-24 15:14:32 +0800

    Add Image Modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,21 +26,22 @@ $(document).on('turbolinks:load', function() {
   var modal = document.getElementById('myModal');
 
   // Get the image and insert it inside the modal - use its "alt" text as a caption
-  var img = document.getElementById('myImg');
-  var modalImg = document.getElementById("img01");
+  var img = $('.myImg');
+  var modalImg = $("#img01");
   var captionText = document.getElementById("caption");
-  img.onclick = function(){
+  $('.myImg').click(function(){
       modal.style.display = "block";
-      modalImg.src = this.src;
+      var newSrc = this.src;
+      modalImg.attr('src', newSrc);
       captionText.innerHTML = this.alt;
-  }
+  });
 
   // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+  var span = document.getElementsByClassName("modal-close")[0];
 
   // When the user clicks on <span> (x), close the modal
-  span.onclick = function() { 
-      modal.style.display = "none";
+  span.onclick = function() {
+    modal.style.display = "none";
   }
 
 
