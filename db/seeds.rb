4938d43ed0ff07e7a85c8043ce92af5d4f495de9
kierra-addison <vinediezca@gmail.com>
2018-08-30 20:13:03 +0800

    Remove Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-admin1 = User.create(first_name: "Geyvin", last_name: "Ediezca", email: "vinediezca@layawsuroy.com", password: "1234567890", password_confirmation: "1234567890", role: "admin")
-admin2 = User.create(first_name: "Bernie", last_name: "Itang", email: "bernieitang@layawsuroy.com", password: "1234567890", password_confirmation: "1234567890", role: "admin")
-admin3 = User.create(first_name: "Jensen", last_name: "Jumatay", email: "jensenjumatay@layawsuroy.com", password: "1234567890", password_confirmation: "1234567890", role: "admin")
-admin3 = User.create(first_name: "Rick Brian", last_name: "Espena", email: "rbespena@layawsuroy.com", password: "1234567890", password_confirmation: "1234567890", role: "admin")
