3e82dd25f60ede493cd6e6aeee006f8021b257b5
kierra-addison <vinediezca@gmail.com>
2018-10-02 21:06:06 +0800

    Add Scroll Top Button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require_tree .
 //= require jquery
 //= require bootstrap-sprockets
+//= require scroll-button
 
 
 
@@ -45,4 +46,30 @@ $(document).on('turbolinks:load', function() {
   };
 
 
+  // Add smooth scrolling to all links
+  $("#scroll-top").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+    
+  });
+
 });
+
+
