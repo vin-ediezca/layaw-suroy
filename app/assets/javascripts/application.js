8fa33423333ca9e2140a6734405e4b6bb98e7e86
kierra-addison <vinediezca@gmail.com>
2018-10-12 20:10:03 +0800

    Change Js File Name 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 //= require_tree .
 //= require jquery
 //= require bootstrap-sprockets
-//= require raw
+//= require scroll-button
 
 
 
@@ -73,3 +73,25 @@ $(document).on('turbolinks:load', function() {
 });
 
 
+
+
+
+
+// Image Slider
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("image-slide");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
