900e5f34ff51a6615f02330851c9b6b5b72a8fb7
kierra-addison <vinediezca@gmail.com>
2018-10-13 15:21:12 +0800

    Add Share Button

--- app/assets/javascripts/raw.js
+++ app/assets/javascripts/raw.js
@@ -4,7 +4,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
     document.getElementById("scroll-top").style.display = "block";
+    document.getElementById("share").style.display = "block";
   } else {
     document.getElementById("scroll-top").style.display = "none";
+    document.getElementById("share").style.display = "none";
   }
 }
